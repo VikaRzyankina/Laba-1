03e1593b3b7458edcc35b40e871886c12edecdca
VikaRzyankina <124519094+VikaRzyankina@users.noreply.github.com>
2023-02-20 21:40:36 +0400

    Update laba1.py

--- laba1.py
+++ laba1.py
@@ -1,65 +1,41 @@
-start_numb = ['1','2','3','4','5','6','7','8','9'] # с чего может начинаться число
-num = ['1','2','3','4','5','6','7','8','9','0']# возможные числа после второго по порядку числа
-lines = []# строки из файла
-numbers = []# числа из файла
-
+#Натуральные числа, содержащие не более двух групп из 5 подряд идущих 0.
+#Список используемых в числах цифр выводить отдельно прописью.
 def repl(n):
-    f = {0:'ноль', 1:'один', 2:'два', 3:'три', 4:'четыре', 5:'пять', 6:'шесть', 7:'семь', 8:'восемь', 9:'девять'}
-    return f.get(n) #замена из словаря
-
-with open('text.txt') as f: #открытие и чтение файла
-    file = f.read()
-    for i in file.split():
-        lines.append(i)
-
-for line in lines: #поиск чисел в файле
-    h = 0
-    for i in range(len(line)):
-        if h > 0:
-            h -= 1
-            break
-        else:
-            if line[i] in start_numb:
-                number = line[i]
-                h = 1
-                while True:
-                    try:
-                        if line[i + h] in num:
-                            number += line[i + h]
-                        if line[i + h] not in num:
-                            break
-                        h += 1
-                    except IndexError:
-                        break
-                numbers.append(number)
-
-if len(numbers) == 0:
-    print('В файле нет чисел')
-    quit()
-
-uwu = [] # подх. по условию
-for i in numbers:
-    kn = 0
-    pn = 0
-    for j in range(len(i)):
-        try:
-            if i[j] == '0':
-                kn += 1
-                if kn == 5 :
-                    pn += 1
-                    kn = 0
-            elif i[j]!='0':
-                kn=0
-
-        except IndexError:
-            break
-    if pn in [1, 2]:
-        uwu.append(i)
-
+    f = {0: 'ноль', 1: 'один', 2: 'два', 3: 'три', 4: 'четыре', 5: 'пять', 6: 'шесть', 7: 'семь', 8: 'восемь', 9: 'девять'}
+    return f.get(n)  # замена из словаря
+uwu=[]
+buffer_len=1
+work_buffer=''
+work_buffer_len=buffer_len
+with open('text.txt','r') as f:
+    buffer=f.read(buffer_len)
+    if not buffer:
+        print('Файл пустой')
+    while buffer:
+        while buffer>='0' and buffer <='9':
+            work_buffer+=buffer
+            buffer=f.read(buffer_len)
+        if work_buffer:
+            kn = 0
+            pn = 0
+            for j in range(len(work_buffer)):
+                try:
+                    if work_buffer[j] == '0':
+                        kn += 1
+                        if kn == 5:
+                            pn += 1
+                            kn = 0
+                    elif work_buffer[j] != '0':
+                        kn = 0
+                except IndexError:
+                    break
+            if pn in [0, 2]:
+                uwu.append(work_buffer)
+        work_buffer=''
+        buffer=f.read(buffer_len)
 if len(uwu) == 0:
     print('В файле нет подходящих под условие чисел')
     quit()
-
 for j in uwu: #замена чисел прописью
     exp = []
     for i in j:
