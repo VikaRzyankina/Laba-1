7f1122fd27aa4dd659181a195356802ef20fdacd
VikaRzyankina <124519094+VikaRzyankina@users.noreply.github.com>
2023-02-21 09:51:21 +0400

    Update laba1.py

--- laba1.py
+++ laba1.py
@@ -4,17 +4,17 @@ def repl(n):
     f = {0: 'ноль', 1: 'один', 2: 'два', 3: 'три', 4: 'четыре', 5: 'пять', 6: 'шесть', 7: 'семь', 8: 'восемь', 9: 'девять'}
     return f.get(n)  # замена из словаря
 uwu=[]
-buffer_len=1
-work_buffer=''
-work_buffer_len=buffer_len
-with open('text.txt','r') as f:
-    buffer=f.read(buffer_len)
-    if not buffer:
+buffer_len=1 #размер буфера чтения
+work_buffer='' #рабочий буфер
+work_buffer_len=buffer_len #длина рабочего буфера
+with open('text.txt','r') as f: #открываем файл
+    buffer=f.read(buffer_len) #читаем первый блок
+    if not buffer: #если пуст
         print('Файл пустой')
-    while buffer:
-        while buffer>='0' and buffer <='9':
+    while buffer: #пока файл не пуст
+        while buffer>='0' and buffer <='9': #обработка текущего блока
             work_buffer+=buffer
-            buffer=f.read(buffer_len)
+            buffer=f.read(buffer_len) #читаем следующий блок
         if work_buffer:
             kn = 0
             pn = 0
@@ -32,7 +32,7 @@ with open('text.txt','r') as f:
             if pn in [0, 2]:
                 uwu.append(work_buffer)
         work_buffer=''
-        buffer=f.read(buffer_len)
+        buffer=f.read(buffer_len) #читаем очередной блок
 if len(uwu) == 0:
     print('В файле нет подходящих под условие чисел')
     quit()
